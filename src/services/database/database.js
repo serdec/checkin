a4c9bf8b7008efd0af36048bd28904dc8a6f847b
Sergio de Cristofaro <s.decristofaro@unitn.it>
2020-09-25 08:25:18 +0200

    made teams collection independent of users

--- src/services/database/database.js
+++ src/services/database/database.js
@@ -4,6 +4,7 @@ import 'firebase/firestore';
 import firebaseConfig from './firebase.config';
 
 const USERS_COLLECTION = 'USERS';
+const TEAMS_COLLECTION = 'TEAMS';
 const CHECKINS = 'CHECKINS';
 const TEAMS = 'TEAMS';
 
@@ -22,16 +23,14 @@ const init = () => {
 };
 init();
 
-export const getTeams = async (user) => {
+export const getTeams = async () => {
   let response = {};
   let data = [];
   await db
-    .collection(USERS_COLLECTION)
-    .doc(user)
-    .collection(TEAMS)
+    .collection(TEAMS_COLLECTION)
     .get()
-    .then(function (querySnapshot) {
-      querySnapshot.forEach(function (doc) {
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
         console.log(doc.id, ' => ', doc.data());
         data.push(doc.data());
@@ -40,27 +39,60 @@ export const getTeams = async (user) => {
       response.payload = data;
     })
     .catch((error) => {
-      console.log(`Error fetching teams, error: ${error}`);
+      console.log('Error fetching teams, error: ', error);
       response.status = 500;
     });
 
   return response;
 };
 
-export const saveTeam = (action) => {
-  db.collection(USERS_COLLECTION)
-    .doc(action.payload.user)
-    .collection(TEAMS)
-    .doc(action.payload.id)
+const updateMembers = async ({ user, name, id }) => {
+  let response = {};
+  await db
+    .collection(USERS_COLLECTION)
+    .doc(user)
+    .collection(TEAMS_COLLECTION)
+    .doc(name)
+    .set(
+      {
+        name,
+        id,
+      },
+      { merge: true }
+    )
+    .then(() => {
+      response.status = 200;
+    })
+    .catch((error) => {
+      console.log(`Error updating team member ${user}, error: ${error}`);
+      response.status = 500;
+    });
+};
+
+export const saveTeam = async (action) => {
+  let saveTeamResponse = {};
+  let updateMembersResponse = {};
+
+  await db
+    .collection(TEAMS_COLLECTION)
+    .doc(action.payload.name)
     .set({
       ...action.payload,
     })
     .then(function () {
       console.log('Document written', action.payload);
+      saveTeamResponse.status = 200;
     })
     .catch(function (error) {
       console.error('Error adding document: ', error);
+      saveTeamResponse.status = 500;
     });
+
+  updateMembersResponse = updateMembers(action.payload);
+
+  if (updateMembersResponse !== 200) return updateMembersResponse;
+
+  return saveTeamResponse;
 };
 
 export const getCheckins = async (user) => {
@@ -81,15 +113,16 @@ export const getCheckins = async (user) => {
       response.payload = data;
     })
     .catch((error) => {
-      console.log('Error getting checkins');
+      console.log('Error getting checkins:', error);
       response.status = 500;
     });
   return response;
 };
-export const saveCheckin = ({ payload }) => {
+export const saveCheckin = async ({ payload }) => {
   let response = {};
 
-  db.collection(USERS_COLLECTION)
+  await db
+    .collection(USERS_COLLECTION)
     .doc(payload.user)
     .collection(CHECKINS)
     .doc(payload.id)
@@ -98,11 +131,11 @@ export const saveCheckin = ({ payload }) => {
     })
     .then(function () {
       console.log('Document written', payload);
-      response.status = '200';
+      response.status = 200;
     })
     .catch(function (error) {
       console.error('Error adding document: ', error);
-      response.status = '500';
+      response.status = 500;
     });
 
   return response;
