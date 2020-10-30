343257976aae0212cbf9a78134b6bcb1f5121731
Sergio de Cristofaro <sdecri@gmail.com>
2020-10-30 19:11:32 +0100

    minor refactoring

--- src/pages/login.js
+++ src/pages/login.js
@@ -65,7 +65,13 @@ const Login = ({
 export const getStaticProps = () => {
   return {
     props: {
-      dbConfig: firebaseConfig,
+      apiKey: process.env.FIREBASE_APIKEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DB_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESS_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
     },
   };
 };
