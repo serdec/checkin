86d0ba20406335c8497d414e322808c2727c7531
Sergio de Cristofaro <sdecri@gmail.com>
2020-10-30 19:34:04 +0100

    Revert "minor refactoring"
    This reverts commit 343257976aae0212cbf9a78134b6bcb1f5121731.

--- src/pages/login.js
+++ src/pages/login.js
@@ -65,13 +65,7 @@ const Login = ({
 export const getStaticProps = () => {
   return {
     props: {
-      apiKey: process.env.FIREBASE_APIKEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DB_URL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESS_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
+      dbConfig: firebaseConfig,
     },
   };
 };
